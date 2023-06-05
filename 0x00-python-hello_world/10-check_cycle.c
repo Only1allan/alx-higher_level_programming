4e73fbe9aec27182a0d67d1428206fd9b66d3e47
Only1allan <kariukiallan850@gmail.com>
2023-06-05 23:24:35 +0300

    add task 11

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ * check_cycle - a function that checks if a linked list has a recurring cycle
+ * list: a pointer to the linked list in question
+ *Return: 1 if a cycle is found and 0 if a failure is not found
+ */
+int check_cycle(listint_t *list)
+{
+  listint_t *current;
+  listint_t *power;
+
+  current = list;
+  power = list;
+
+  while (current != NULL && power != NULL)
+  {
+    current = current->next;
+    power = power->next;
+
+    if (current == power)
+      return (1);
+    else
+      return (0);
+  }
+  return (0);
+}
+    
+    
+      
