0458f818c21bd78d09d17c8dccdefade6e4df484
Only1allan <kariukiallan850@gmail.com>
2023-06-08 21:33:09 +0300

    add task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-from sys import argv
-summ = 0
-for i in range(argv[1:]):
-    summ += int(i)
-print("{:d}".format(summ))
+if __name__ = "__main__":
+    from sys import argv
+    summ = 0
+    for i in range(argv[1:]):
+        summ += int(i)
+    print("{:d}".format(summ))
