01b8f9049a808b612fa8bc3e28218ab3adf770b7
Only1allan <kariukiallan850@gmail.com>
2023-06-08 21:22:26 +0300

    add task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+argsl = len(sys.argv) - 1
+if argsl == 0:
+    print("0 arguments.")
+elif argsl == 1:
+    print("1 argument:")
+else:
+    print("{} arguments:".format(argsl))
+for i in range(argsl):
+    print("{}: {}".format(i + 1, sys.argv[i + 1]))
