6886c8e6cb54fdedb7fe03872a683782c75bc632
Only1allan <kariukiallan850@gmail.com>
2023-06-15 02:52:20 +0300

    add task 10

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+xcx#!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     new_d = {}
     for i in a_dictionary:
---
+++ 0x04-python-more_data_structures/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    return a_dictionary, key=a_dictionary.get
+    
+def main:
+    best_score = __import__('10-best_score').best_score
+
+    a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
+    best_key = best_score(a_dictionary)
+    print("Best score: {}".format(best_key))
+
+    best_key = best_score(None)
+    print("Best score: {}".format(best_key))
+    
+main()
