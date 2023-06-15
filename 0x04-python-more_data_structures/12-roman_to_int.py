713ca1a7d31f8a4ef454920639fc8ec773cdf81e
Only1allan <kariukiallan850@gmail.com>
2023-06-15 03:25:48 +0300

    add task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    roman_values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_values = {'M': 1000,
+                    'D': 500,
+                    'C': 100,
+                    'L': 50,
+                    'X': 10,
+                    'V': 5,
+                    'I': 1
+                    }
     result = 0
     prev_value = 0
 
