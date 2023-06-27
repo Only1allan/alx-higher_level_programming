acb4082e66c13fe844e32e3274fe0cc1bd1b872a
Only1allan <kariukiallan850@gmail.com>
2023-06-27 17:12:01 +0300

    add task 0

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Define a square"""
+
+
+class Square:
+    """
+    A class representations of a square
+
+    Attribute:
+    None
+    Methods:
+    None
+
+    """
+    pass
