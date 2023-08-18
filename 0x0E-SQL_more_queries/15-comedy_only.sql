e90ad7aaf2317bacd8f008583e157978550d2309
Only1allan <kariukiallan850@gmail.com>
2023-08-18 05:51:19 +0300

    add more sql queries

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -2,8 +2,8 @@
 SELECT tv_shows.title 
 FROM tv_shows
 INNER JOIN tv_show_genres
-ON tv_show_genres,show_id = tv_show.id
+ON tv_show_genres.show_id = tv_shows.id
 INNER JOIN tv_genres
-ON tv_genres.id  tv_show_genres.genre_id
+ON tv_genres.id = tv_show_genres.genre_id
 WHERE tv_genres.name = "Comedy"
-ORDER BY tv_show.title;
+ORDER BY tv_shows.title;
