457ae9702bbefd8efe8cf6f3271a29eac8bb0063
Only1allan <kariukiallan850@gmail.com>
2023-09-12 03:05:54 +0300

    finished mandatory

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 function factorial (n) {
-return n === 0 || isNaN(n) ? 1 : n *factorial(n - 1);
+  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
 }
 const args = parseInt(process.argv[2]);
 const result = factorial(args);
--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-n = process.argv.slice(2); 
-console.log(n <= 3 ? "0" : search(n));
+const n = process.argv.slice(2);
+console.log(n <= 3 ? '0' : search(n));
 
-function search(n){
-   n.sort((a, b) => b - a);
-   if (n.length >= 2){
+function search (n) {
+  n.sort((a, b) => b - a);
+  if (n.length >= 2) {
     return n[1];
-   }
+  }
 }
