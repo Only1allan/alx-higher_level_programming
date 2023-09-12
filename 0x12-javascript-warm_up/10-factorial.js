f007fb57e63bab60f7edaa1a5ac40cdc4847412d
Only1allan <kariukiallan850@gmail.com>
2023-09-12 03:03:00 +0300

    finished mandatory

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
 function factorial (n) {
-  if (n === 0 || n === 1) {
-    return 1;
-  } else {
-    return n * (factorial(n - 1));
-  }
+return n === 0 || isNaN(n) ? 1 : n *factorial(n - 1);
 }
 const args = parseInt(process.argv[2]);
 const result = factorial(args);
