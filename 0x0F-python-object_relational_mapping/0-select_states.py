0b2f8fc7ff633c4d65f632c124ee6fab130be93d
Only1allan <kariukiallan850@gmail.com>
2023-09-16 09:17:11 +0300

    add task 1

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,15 +6,27 @@ import MySQLdb
 import sys
 
 if __name__ == '__main__':
-    database = MySQLdb.connect(
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        database=sys.argv[3],
-        port=3306
-    )
-    cursor = database.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY states.id")
-    states = cursor.fetchall()
+    if len(sys.argv) != 4:
+        print("Invalid argument number")
+        sys.exit(1)
+    try:
 
-    for state in states:
-        print(state)
+        if __name__ == '__main__':
+            database = MySQLdb.connect(
+                user=sys.argv[1],
+                passwd=sys.argv[2],
+                database=sys.argv[3],
+                port=3306
+            )
+            cursor = database.cursor()
+            cursor.execute("SELECT * FROM states ORDER BY states.id")
+            states = cursor.fetchall()
+
+            for state in states:
+                print(state)
+    except MySQLdb.Error as error:
+        print("Could not connect to server")
+
+    finally:
+        cursor.close()
+        database.close()
---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+script listing all states with a name starting with N (upper N)
+"""
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        print("Invalid argument number")
+        sys.exit(1)
+
+    try:
+        database = MySQLdb.connect(
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            database=sys.argv[3],
+            port=3306
+        )
+        cursor = database.cursor()
+        cursor.execute("SELECT * \
+         FROM states \
+         WHERE name LIKE 'N%'\
+         ORDER BY states.id")
+        states = cursor.fetchall()
+
+        for state in states:
+            print(state)
+
+    except MySQLdb.Error as error:
+        print("Could not connect to server")
+
+    finally:
+        cursor.close()
+        database.close()
