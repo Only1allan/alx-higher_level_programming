9b57f59b617372306e9572e4dbdda1bb7f418096
Only1allan <kariukiallan850@gmail.com>
2023-09-17 21:17:06 +0300

    added more ORM task

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -5,30 +5,23 @@ script listing all states from the database hbtn_0e_0_usa
 if __name__ == '__main__':
     import MySQLdb
     import sys
-    if len(sys.argv) != 4:
-        print("Invalid argument number")
-        sys.exit(1)
 
-    try:
-        database = MySQLdb.connect(
-                user=sys.argv[1],
-                passwd=sys.argv[2],
-                database=sys.argv[3],
-                host="localhost",
-                port=3306
-            )
-        cursor = database.cursor()
-        cursor.execute("SELECT cities.id, cities.name, states.name \
-                    FROM cities\
-                    INNER JOIN states ON cities.states_id=states.id \
-                    ORDER BY states.id")
-        states = cursor.fetchall()
+    database = MySQLdb.connect(
+            host="localhost",
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            database=sys.argv[3],
+            port=3306
+        )
+    cursor = database.cursor()
+    cursor.execute("SELECT cities.id, cities.name, states.name \
+                FROM cities\
+                JOIN states ON cities.states_id=states.id\
+                ORDER BY cities.id")
+    states = cursor.fetchall()
 
-        for state in states:
-            print(state)
-    except MySQLdb.Error as error:
-        print("Could not connect to server")
+    for state in states:
+        print(state)
 
-    finally:
-        cursor.close()
-        database.close()
+    cursor.close()
+    database.close()
--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -6,34 +6,25 @@ import MySQLdb
 import sys
 
 if __name__ == '__main__':
-    if len(sys.argv) != 5:
-        print("Invalid argument number")
-        sys.exit(1)
-    cursor = None
+    database = MySQLdb.connect(
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            database=sys.argv[3],
+            port=3306
+        )
+    cursor = database.cursor()
+    match = sys.argv[4]
+    query = "SELECT cities.name \
+                    FROM cities\
+                    INNER JOIN states ON (states.id = cities.state_id)\
+                    WHERE state.name=%s \
+                    ORDER BY states.id"
+    cursor.execute(query, (sys.argv[4], ))
+    cities = cursor.fetchall()
 
-    try:
-        database = MySQLdb.connect(
-                user=sys.argv[1],
-                passwd=sys.argv[2],
-                database=sys.argv[3],
-                port=3306
-            )
-        cursor = database.cursor()
-        match = sys.argv[4]
-        query = "SELECT cities.name \
-                        FROM cities\
-                        INNER JOIN states ON (states.id = cities.state_id)\
-                        WHERE state.name=%s \
-                        ORDER BY states.id"
-        cursor.execute(query, (sys.argv[4], ))
-        cities = cursor.fetchall()
+    city_names = [city[0]for city in cities]
+    city_total = ", ".join(city_names)
+    print(city_total)
 
-        city_names = [city[0]for city in cities]
-        city_total = ", ".join(city_names)
-        print(city_total)
-    except MySQLdb.Error as error:
-        print("Could not connect to server")
-
-    finally:
-        cursor.close()
-        database.close()
+    cursor.close()
+    database.close()
