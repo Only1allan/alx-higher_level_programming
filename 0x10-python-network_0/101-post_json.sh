e9bfcf85e7b800b028e16461c240dd6b228a0da4
only1allan <kariukiallan850@gmail.com>
2023-09-28 19:45:19 -0700

    fixed errors

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #script that send JSON POST and displays body of response
-curl -s "$1" -X POST -H "Content-Type: application/json" -d @"$2"
+curl -s "$1" -X POST -H -d @"$2" "Content-Type: application/json"
