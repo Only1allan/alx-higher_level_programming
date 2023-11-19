1ad06ced6d43bc6d71e6e5dc30dd8311772030e9
dev254 <dev254@pop-os.localdomain>
2023-11-19 10:38:01 +0300

    add selct state script

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,12 +3,17 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
+
+	if len(sys.argv) != 4:
+		print("Missing arguments")
+		sys.exit(1);
+
 	db = MySQLdb.connect(
 		host="localhost",
 		port = 3306,
 		user=sys.argv[1],
 		passwd=sys.argv[2],
-		db=sys.argv[3]
+		db=sys.argv[3])
 
 	cur = db.cursor()
 
