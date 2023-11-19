0278f824c2b72721d697bae9ac8fd21612e1ca5d
Only1allan <kariukiallan850@gmail.com>
2023-11-19 21:53:13 +0300

    fixed code

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-# script listing all states from the database hbtn_0e_0_usa
-
+"""
+script listing all states from the database hbtn_0e_0_usa
+"""
 
 if __name__ == "__main__":
     import MySQLdb
