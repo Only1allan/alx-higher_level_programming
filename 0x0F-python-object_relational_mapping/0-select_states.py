42117cf02c515f7af48479825c9a79c1f3a3f68f
Only1allan <kariukiallan850@gmail.com>
2023-11-19 21:44:28 +0300

    fixed code

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 # script listing all states from the database hbtn_0e_0_usa
-import MySQLdb
-import sys
+
 
 if __name__ == "__main__":
+    import MySQLdb
+    import sys
 
     if len(sys.argv) != 4:
         print("Missing arguments")
