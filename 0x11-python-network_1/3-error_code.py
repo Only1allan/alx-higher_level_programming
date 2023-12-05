12b21e0560224c40c5f5dcc387b8d693db0c32df
Only1allan <kariukiallan850@gmail.com>
2023-12-05 22:54:11 +0300

    Display body of response

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""script takes in a URL, sends a request and displays body of response"""
+import urllib.request
+import urllib.error
+import sys
+
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as response:
+            print(response.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
