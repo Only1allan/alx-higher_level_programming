85377eec3fb70677c70b7ad9b4d2d6aca0a6e102
Only1allan <kariukiallan850@gmail.com>
2023-12-05 18:56:02 +0300

    fetch data from a url

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# script that fetches https://intranet.hbtn.io/status
+import urllib.request
+
+if __name__ == "__main__":
+    url = "https://intranet.hbtn.io/status"
+    req = urllib.request.Request(url)
+
+    try:
+        with urllib.request.urlopen(req) as response:
+            html = response.read()
+            print("Body response:")
+            print("\t- type: {}".format(type(html)))
+            print("\t- content: {}".format(html))
+            print("\t- utf8 content: {}".format(html.decode('utf-8')))
+    except urllib.error.HTTPError as e:
+        print("Error: {}".format(e))
---
+++ 0x11-python-network_1/README.md
@@ -0,0 +1,1 @@
+A directory with Python Networking scripts using the url module
