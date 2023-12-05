fe622acafaceb3879b5c2403409528e722329e02
Only1allan <kariukiallan850@gmail.com>
2023-12-05 19:14:29 +0300

    refine code

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-""" script that fetches https://intranet.hbtn.io/status """
+""" script that fetches https://alx-intranet.hbtn.io/status """
 import urllib.request
 
 if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
-        AppleWebKit/537.36 (KHTML, like Gecko) \
-            Chrome/91.0.4472.124 Safari/537.36'}
-    req = urllib.request.Request(url, headers=headers)
+    req = urllib.request.Request(url)
 
     with urllib.request.urlopen(req) as response:
         html = response.read()
