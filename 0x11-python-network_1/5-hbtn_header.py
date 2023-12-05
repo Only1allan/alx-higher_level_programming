69ab82378effa4ccab51a62e7f7aab785ea286e9
Only1allan <kariukiallan850@gmail.com>
2023-12-05 23:28:22 +0300

    View the varibale served by

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""script takes in a URL, sends a request and displays value of X-Request-Id"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    response = requests.get(url)
+    print(response.headers.get('X-Request-Id'))
