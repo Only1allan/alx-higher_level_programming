42ea5f0b30c20e1c1c3da3059d8975684c4e2aeb
Only1allan <kariukiallan850@gmail.com>
2023-12-05 22:47:56 +0300

    Fixed error on email paramter

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -6,7 +6,9 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    data = {'mail': sys.argv[2]}
+    email = sys.argv[2]
+
+    data = {'email': email}
 
     data = urllib.parse.urlencode(data).encode('ascii')
     req = urllib.request.Request(url, data)
