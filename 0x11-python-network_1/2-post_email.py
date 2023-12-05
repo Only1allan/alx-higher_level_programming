e086a1c618d39428bd2cdd4e5c0f064a853b15ac
Only1allan <kariukiallan850@gmail.com>
2023-12-05 22:32:00 +0300

    Post data to a server

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""script takes in a URL and an email address, sends a POST request"""
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    data = {'mail': sys.argv[2]}
+
+    data = urllib.parse.urlencode(data).encode('ascii')
+    req = urllib.request.Request(url, data)
+
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode('utf-8'))
