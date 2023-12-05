2e427417c9abcb64c96069553013b73b588b9931
Only1allan <kariukiallan850@gmail.com>
2023-12-05 19:13:04 +0300

    port error

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,7 +3,7 @@
 import urllib.request
 
 if __name__ == "__main__":
-    url = "https://intranet.hbtn.io/status"
+    url = "https://alx-intranet.hbtn.io/status"
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
         AppleWebKit/537.36 (KHTML, like Gecko) \
             Chrome/91.0.4472.124 Safari/537.36'}
