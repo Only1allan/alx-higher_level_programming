b713fcf050bea488adcf818356168495e3e25372
Only1allan <kariukiallan850@gmail.com>
2023-12-05 19:07:15 +0300

    fix 403 forbidden  error

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,14 +4,14 @@ import urllib.request
 
 if __name__ == "__main__":
     url = "https://intranet.hbtn.io/status"
-    req = urllib.request.Request(url)
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
+        AppleWebKit/537.36 (KHTML, like Gecko) \
+            Chrome/91.0.4472.124 Safari/537.36'}
+    req = urllib.request.Request(url, headers=headers)
 
-    try:
-        with urllib.request.urlopen(req) as response:
-            html = response.read()
-            print("Body response:")
-            print("\t- type: {}".format(type(html)))
-            print("\t- content: {}".format(html))
-            print("\t- utf8 content: {}".format(html.decode('utf-8')))
-    except urllib.error.HTTPError as e:
-        print("Error: {}".format(e))
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
