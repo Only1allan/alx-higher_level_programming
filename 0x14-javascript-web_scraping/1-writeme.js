d80d126611b0b3b1f20b904a2ef2de22716028e8
Only1allan <kariukiallan850@gmail.com>
2024-01-30 23:15:58 +0300

    write content into  file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
+// Writes a string to a file.
+
 const fs = require('fs');
 const file = process.argv[2];
-const content = process.argv[3];
 
-fs.writeFile(file, content, 'utf-8', function (error) {
-  if (error) {
-    console.log(error);
+fs.writeFile(file, process.argv[3], 'utf8', function (err, data) {
+  if (err) {
+    return console.log(err);
   }
 });
